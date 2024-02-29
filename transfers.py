f506d742ebcc3aadde4541b9f6d64fa76275a1ba
Drake Wilcox <drakewilcox@gmail.com>
2024-02-29 11:45:50 -0800

    update transfers and actions yml.

--- transfers.py
+++ transfers.py
@@ -4,28 +4,24 @@ TRANSFERS = [
     "from_playlist_link": "https://open.spotify.com/playlist/7tOT1UK2vnqTpWbwmuJs1a?si=e29942bd652c42a9",
     "to_playlist_name": "New Commute 2024 Archive",
     "to_playlist_link": "https://open.spotify.com/playlist/1KHuoPDQnNWPpW9UDtbZxP?si=426c0f52e0f14cc2",
-    "update_frequency": "",
   },
     {
     "from_playlist_name": "Pollen",
     "from_playlist_link": "https://open.spotify.com/playlist/37i9dQZF1DWWBHeXOYZf74?si=460546a70dd74d0f",
     "to_playlist_name": "Pollen Archive",
     "to_playlist_link": "https://open.spotify.com/playlist/6WEshQCCEtwL46n7aX8KzA?si=50d72ac9b36e46e1",
-    "update_frequency": "",
   },
     {
     "from_playlist_name": "Compact Cassette",
     "from_playlist_link": "https://open.spotify.com/playlist/6c6Ra1iI1JjhADV2gnT7p6?si=b8322658ffc34f10",
     "to_playlist_name": "Compact Cassette Archive",
     "to_playlist_link": "https://open.spotify.com/playlist/1Z4mPhioxV0U1dMqiCxs2r?si=55c47938ac854674",
-    "update_frequency": "",
   },
   {
     "from_playlist_name": "2024 - Favorite Tracks (So Far)",
     "from_playlist_link": "https://open.spotify.com/playlist/6Db7AUrNyw93xs9fnZKabQ?si=532714784b524fc0",
     "to_playlist_name": "2024 Favorite Tracks Archive",
     "to_playlist_link": "https://open.spotify.com/playlist/1Z4mPhioxV0U1dMqiCxs2r?si=55c47938ac854674",
-    "update_frequency": "",
   },
 
 ]
