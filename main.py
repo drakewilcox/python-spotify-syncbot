b3b9f5c9582e9c0267dbbcd47e654f7ad428d031
Drake Wilcox <drakewilcox@gmail.com>
2024-03-05 12:59:55 -0800

    remove mistaken arguemnt from playlist creation.

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ if DAYLIST["sync_on"]:
   existing_archive = sp.search_playlists_by_name(query=archive_name)
 
   if existing_archive:
-    sp.transfer_songs_to_archive(from_playlist=daylist, to_playlist=existing_archive)
+    sp.transfer_songs_to_archive(from_playlist=daylist, to_playlist=existing_archive, is_daylist=True)
   else:
-    new_archive = sp.user_playlist_create(user=sp.current_user_id, name=archive_name, public=False, is_daylist=True)
+    new_archive = sp.user_playlist_create(user=sp.current_user_id, name=archive_name, public=False)
     sp.transfer_songs_to_archive(from_playlist=daylist, to_playlist=new_archive, is_new_archive=True, is_daylist=True)
 
 # GENERAL TRANSFERS SYNC
